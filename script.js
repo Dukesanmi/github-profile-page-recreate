47fc8e20d20ce1e300236acffa5f44e388e7c4b0
Oluwasanmi <42076983+Dukesanmi@users.noreply.github.com>
2021-06-03 13:34:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const log = console.log;
-const githubToken = JSON.Stringify(process.env.PERSONAL_ACCESS_TOKEN);
-/*const variables = {
-  "githubToken": JSON.Stringify(process.env.PERSONAL_ACCESS_TOKEN),
+//const githubToken = JSON.Stringify(process.env.PERSONAL_ACCESS_TOKEN);
+const variables = {
+  "githubToken": process.env.GITHUB_TOKEN,
   "githubLogin": "Dukesanmi",
   //reposToFetch: 20
-}*/
+}
 //const fetch = require('node-fetch');
 const queryRepo = {
  "query": `
